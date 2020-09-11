e14d82da1416651da845daf8025225218148cbfe
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-09-11 16:40:55 -0400

    have form and app.js created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css'
+import React, { useState } from "react"
+import { Switch, Route } from "react-router-dom"
+
+import "./App.css"
+
+import Layout from "./layouts/Layout"
+import Login from "./screens/Login"
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  const loginSubmit = () => {}
+
   return (
-    <div className="App">
-    </div>
-  );
+    <Layout>
+      <Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/register"></Route>
+        <Route exact path="/"></Route>
+      </Switch>
+    </Layout>
+  )
 }
 
-export default App;
+export default App
